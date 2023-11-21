d11a42e18ad08b357abc029ac13fb5b0a84c4cd5
romain <rousvalromain@gmail.com>
2023-11-21 11:45:03 +0100

    modif gestion formation

--- EtuUU/src/MainFormation.java
+++ EtuUU/src/MainFormation.java
@@ -2,23 +2,13 @@ import formation.*;
 
 public class MainFormation {
 
-  static void test1(GestionFormation e) {
+  static void test1() {
     GestionFormation ge=new GestionFormation();
-    e.creerFormation("ki", "lo", "ju");
-    e.creerFormation("ko", "li", "ja");
-    e.creerFormation("ki", "lo", "ju");
-    e.ccc();
-    e.definirNombreOptions(9);
-    e.definirNombreOptions(15);
-    e.definirNombreOptions(86);
-    e.setTailleGroupeDirige(20);
-    e.setTailleGroupePratique(26);
-    System.out.println("--------------------------------------------");
-    ge.ccc();
-    System.out.println(e);
+
+
   }
 
   public static void main(String[] args) {
-    test1(new GestionFormation("lol", "huk", "ju"));
+    test1();
   }
 }
