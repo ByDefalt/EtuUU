fc006d379ba23ef25045666d0ff9f84099fa812a
ByDefalt <rousvalromain@gmail.com>
2024-01-08 16:11:51 +0100

    Update FormationControleur.java

--- src/ui/FormationControleur.java
+++ src/ui/FormationControleur.java
@@ -4,9 +4,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import formation.GestionFormation;
+import formation.UniteEnseignement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -21,179 +21,189 @@ import javafx.scene.input.MouseEvent;
  * @author Eric Cariou
  */
 public class FormationControleur {
-  
+
   @FXML
   private ResourceBundle resources;
-  
+
   @FXML
   private URL location;
-  
+
   @FXML
   private CheckBox checkInscriptionFinalisee;
-  
+
   @FXML
   private TextField entreeAdresseEtudiant;
-  
+
   @FXML
   private TextField entreeAgeEtudiant;
-  
+
   @FXML
   private TextField entreeCapaciteAccueil;
-  
+
   @FXML
   private TextField entreeEmailResponsableFormation;
-  
+
   @FXML
   private TextField entreeGroupeTDEtudiant;
-  
+
   @FXML
   private TextField entreeGroupeTPEtudiant;
-  
+
   @FXML
   private TextField entreeNomEtudiant;
-  
+
   @FXML
   private TextField entreeNomFormation;
-  
+
   @FXML
   private TextField entreeNomResponsableFormation;
-  
+
   @FXML
   private TextField entreeNomResponsableUE;
-  
+
   @FXML
   private TextField entreeNomUE;
-  
+
   @FXML
   private TextField entreeNombreChoixOptions;
-  
+
   @FXML
   private TextField entreePrenomEtudiant;
-  
+
   @FXML
   private TextField entreeTailleGroupeTD;
-  
+
   @FXML
   private TextField entreeTailleGroupeTP;
-  
+
   @FXML
   private Label labelListeEtudiants;
-  
+
   @FXML
   private Label labelNbGroupesTD;
-  
+
   @FXML
   private Label labelNbGroupesTP;
-  
+
   @FXML
   private ListView<String> listeEtudiants;
-  
+
   @FXML
   private ListView<String> listeUEObligatoires;
-  
+
   @FXML
   private ListView<String> listeUEOptionnelles;
-  
+
   @FXML
   private ToggleGroup obligation;
-  
+
   @FXML
   private RadioButton radioBoutonObligatoire;
-  
+
   @FXML
   private RadioButton radioBoutonOptionnelle;
-  
+
+  GestionFormation ges = new GestionFormation();
+
   @FXML
   void actionBoutonAffectationAutomatique(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionBoutonAffectationManuelleGroupes(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionBoutonAfficherEtudiantsGroupeTD(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionBoutonAfficherEtudiantsGroupeTP(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionBoutonAfficherEtudiantsUEOptionnelle(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionBoutonAfficherTousEtudiants(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionBoutonCreerFormation(ActionEvent event) {
-    GestionFormation ges=new GestionFormation();
-    ges.creerFormation(entreeNomFormation.getText(), entreeNomResponsableFormation.getText(), entreeEmailResponsableFormation.getText());
+    ges.creerFormation(entreeNomFormation.getText(), entreeNomResponsableFormation.getText(),
+        entreeEmailResponsableFormation.getText());
   }
-  
+
   @FXML
   void actionBoutonNombreChoixOptions(ActionEvent event) {
-    
+    ges.definirNombreOptions(Integer.parseInt(entreeNombreChoixOptions.getText()));
   }
-  
+
   @FXML
   void actionBoutonSetTailleGroupeTD(ActionEvent event) {
-    
+    ges.setTailleGroupeDirige(Integer.parseInt(entreeTailleGroupeTD.getText()));
+    labelNbGroupesTD.setText(ges.getTailleGroupeDirige() + "");
   }
-  
+
   @FXML
   void actionBoutonSetTailleGroupeTP(ActionEvent event) {
-    
+    ges.setTailleGroupePratique(Integer.parseInt(entreeTailleGroupeTP.getText()));
+    labelNbGroupesTP.setText(ges.getTailleGroupePratique() + "");
   }
-  
+
   @FXML
   void actionMenuApropos(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionMenuCharger(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionMenuQuitter(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionMenuSauvegarder(ActionEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionSelectionEtudiant(MouseEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionSelectionUEObligatoire(MouseEvent event) {
-    
+
   }
-  
+
   @FXML
   void actionSelectionUEOptionnelle(MouseEvent event) {
-    
+
   }
+
   @FXML
   void actionBoutonCreerNouvelleUE(ActionEvent event) {
-    
+    if (radioBoutonObligatoire.isSelected()) {
+      ges.ajouterEnseignementObligatoire(new UniteEnseignement(entreeNomUE.getText(), entreeNomResponsableUE.getText()));
+    } else if (radioBoutonOptionnelle.isSelected()) {
+      ges.ajouterEnseignementOptionnel(new UniteEnseignement(entreeNomUE.getText(), entreeNomResponsableUE.getText()),Integer.parseInt(entreeCapaciteAccueil.getText()));
+    }
   }
+
   @FXML
   void initialize() {
-    
+
   }
 }
