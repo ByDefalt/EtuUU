feae5e472aa19e21a8ddb1cbf21ecf2a6c718de2
ByDefalt <rousvalromain@gmail.com>
2024-01-09 17:37:26 +0100

    doc

--- src/formation/UniteEnseignement.java
+++ src/formation/UniteEnseignement.java
@@ -24,7 +24,9 @@ public class UniteEnseignement {
         this.nomEnseignant = nomEnseignant;
     }
 
-    /** Retoune l'intance optionel
+    /**
+     * Retoune l'intance optionel
+     * 
      * @return
      *         <ul>
      *         <li>True si le l'UE est optionel</li>
@@ -35,28 +37,36 @@ public class UniteEnseignement {
         return Optionnel;
     }
 
-    /** Permet de definir si une UE est optionel ou non
+    /**
+     * Permet de definir si une UE est optionel ou non
+     * 
      * @param Optionnel variable boolean qui permet de mettre l'UE en optionel
      */
     public void setOptionnel(boolean Optionnel) {
         this.Optionnel = Optionnel;
     }
 
-    /** Retoune le nom de l'UE
+    /**
+     * Retoune le nom de l'UE
+     * 
      * @return Le nom de l'UE
      */
     public String getNomUE() {
         return nomUE;
     }
 
-    /** Retourne le nombre de place maximum de l'UE
+    /**
+     * Retourne le nombre de place maximum de l'UE
+     * 
      * @return Le nombre de place maximum de l'UE
      */
     public int getNbPlacesMax() {
         return nbPlacesMax;
     }
 
-    /** Set le nombre de place maximum si l'UE est Optionel
+    /**
+     * Set le nombre de place maximum si l'UE est Optionel
+     * 
      * @param nbPlacesMax Le nombre de place maximum de l'UE
      */
     public void setNbPlacesMax(int nbPlacesMax) {
@@ -66,20 +76,54 @@ public class UniteEnseignement {
     }
 
     /**
-     * @return
+     * Retoune le nom de l'enseignant
+     * 
+     * @return Le nom de l'enseignant
      */
     public String getNomEnseignant() {
         return nomEnseignant;
     }
 
+    /**
+     * Set le nom de l'enseignant
+     * 
+     * @param nomEnseignant Le nom de l'enseignant
+     */
     public void setNomEnseignant(String nomEnseignant) {
         this.nomEnseignant = nomEnseignant;
     }
 
+    /**
+     * Retoune le nombre de Etudiant inscrit à l'UE
+     * 
+     * @return Le nombre de Etudiant inscrit à l'UE
+     */
     public int getnbParticipant() {
         return nbParticipant;
     }
 
+    /**
+     * Incrémente le nombre de participants si l'optionnel est activé.
+     * Si l'optionnel n'est pas activé, le nombre de participants reste inchangé.
+     * 
+     * <p>
+     * Exemple d'utilisation :
+     * 
+     * <pre>{@code
+     * // Création d'une instance de la classe
+     * UniteEnseignement instance = new UniteEnseignement("Java","Richard");
+     * 
+     * // Activation de l'optionnel
+     * instance.setOptionnel(true);
+     * 
+     * // Incrémentation du nombre de participants
+     * instance.setnbParticipant();
+     * }</pre>
+     * </p>
+     * 
+     * @see #setOptionnel(boolean)
+     * @see #getnbParticipant()
+     */
     public void setnbParticipant() {
         if (this.Optionnel) {
             this.nbParticipant += 1;
