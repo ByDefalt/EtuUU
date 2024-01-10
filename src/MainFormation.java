d0a7fcbc036a793e9c79a00a850c39a97021ce60
ByDefalt <rousvalromain@gmail.com>
2024-01-10 20:47:21 +0100

    Update MainFormation.java

--- src/MainFormation.java
+++ src/MainFormation.java
@@ -6,21 +6,7 @@ import formation.*;
 public class MainFormation {
 
   static void test1() throws CloneNotSupportedException {
-    GestionFormation ges=new GestionFormation();
-    ges.creerFormation("hgfd", "hgfd", "hfdg@gdfsg.nugdfsdgll");
-    ges.setTailleGroupeDirige(5);
-    ges.setTailleGroupePratique(5);
-    ges.definirNombreOptions(5);
-    ges.ajouterEnseignementObligatoire(new UniteEnseignement("vfed", "dsgfd"));
-    ges.ajouterEnseignementOptionnel(new UniteEnseignement("vfed", "dsgfd"),10);
-    for(int i=1;i<2;i++){
-      ges.getGestionEtudiant().inscription(new InformationPersonnelle(i+"", i+""), i+"");
-    }
-    ges.attribuerAutomatiquementGroupes();
-    System.out.println(ges+"\n\n");
-    GestionFormation ges2=new GestionFormation();
-    ges2.copierDepuis(ges);
-    System.out.println(ges2);
+    
   }
 
   public static void main(String[] args) throws CloneNotSupportedException {
