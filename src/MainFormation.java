357c89ece81918d1d9305358f8daa5366cc75d0e
ByDefalt <rousvalromain@gmail.com>
2024-01-11 17:49:28 +0100

    Update MainFormation.java

--- src/MainFormation.java
+++ src/MainFormation.java
@@ -1,14 +1,7 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import formation.Etudiant;
-import formation.GestionFormation;
-import formation.InformationPersonnelle;
-import formation.UniteEnseignement;
-
 public class MainFormation {
 
   static void test1() throws CloneNotSupportedException {
-    
+
   }
 
   public static void main(String[] args) throws CloneNotSupportedException {
